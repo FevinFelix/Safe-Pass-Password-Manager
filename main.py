8621d85e0b26ccc1dcce5fb88fc0d2553f338347
fevinfelix <fevinfelix@gmail.com>
2023-01-13 00:08:32 -0500

    recognizes if it's the first time using the password manager and prompts for a new username and password

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import os
 
-
-users = []
+profile = None
 
 class User:
     def __init__(self, username: str, password: str):
@@ -26,3 +25,30 @@ class User:
             self.password_pairs.update({site, new_password})
             return True
         return False
+    
+def main():
+    username = None
+    password = None
+    if os.path.getsize('passwords.txt') == 0:
+        print("It looks like this is your first time using the password manager. Let's set up a profile for you!")
+        while True:
+            username = str(input("Please enter a username without any spaces: "))
+            if not ' ' in username:
+                break
+        while True:
+            password = str(input("Please enter a password without any spaces: "))
+            if not ' ' in password:
+                break
+            
+            
+
+""" def main():
+    print("Welcome to your password manager! What would you like to do?")
+    while True:
+        response = input("\nAdd Password: 'Add'\nRemove Password: 'Remove'\nChange Password: 'Change'\nSelection: ")
+        response = response.lower()
+        if response == 'add' or response == 'remove' or response == 'change':
+            break """ # use option shift a to uncomment
+
+main()
+    
