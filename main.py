efae5d75817bd90bd897e5d5b8943db39e9f8da3
fevinfelix <fevinfelix@gmail.com>
2023-01-13 13:33:14 -0500

    set up user login info and authenticates properly before allowing choices

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import os
 
-profile = None
+profile = {}
+
+if os.path.getsize('passwords.txt') != 0: # if the profile has been set up already, load in the username and password
+    with open("passwords.txt") as f:
+            (key, val) = f.readline().strip('\n').split()
+            profile[key] = val
+
 
 class User:
     def __init__(self, username: str, password: str):
@@ -27,10 +33,8 @@ class User:
         return False
     
 def main():
-    username = None
-    password = None
-    if os.path.getsize('passwords.txt') == 0:
-        print("It looks like this is your first time using the password manager. Let's set up a profile for you!")
+    if len(profile) == 0:
+        print("It looks like you don't have a profile set up. Let's set one up for you!")
         while True:
             username = str(input("Please enter a username without any spaces: "))
             if not ' ' in username:
@@ -39,16 +43,33 @@ def main():
             password = str(input("Please enter a password without any spaces: "))
             if not ' ' in password:
                 break
-            
-            
-
-""" def main():
-    print("Welcome to your password manager! What would you like to do?")
+        # at this point, you have a valid username and password without spaces for the user 
+        # now, add the username and password to the txt file as as key value pair
+        passwords_file = open("passwords.txt", "a")
+        passwords_file.write(username + " " + password)
+        # now the username and the password are in the file for the user
+    else: # grab the username and password for the user from the dictionary
+        username = next(iter(profile))
+        password = profile[username]
+    
     while True:
-        response = input("\nAdd Password: 'Add'\nRemove Password: 'Remove'\nChange Password: 'Change'\nSelection: ")
-        response = response.lower()
-        if response == 'add' or response == 'remove' or response == 'change':
-            break """ # use option shift a to uncomment
+        print()
+        username_entry = str(input("Please enter your username to login: "))
+        password_entry = str(input("Please enter your password to login: "))
+        if username == username_entry and password == password_entry:
+            break
+        print("Incorrect username or password. Please try again.")
+
+    while True:
+        print()
+        response = input(f"Welcome {username}! What would you like to do?:\nAdd A Password: [A]\nChange A Password: [C]\
+        \nRemove A Password: [R]\nSELECT: ").lower()
+        if response == 'a' or response == 'r' or response == 'c':
+            break
+        print("\n******************************")
+        print("Please choose a valid choice.")
+        print("******************************")
+    
 
 main()
     
