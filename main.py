acd95a2d99a55ab57a090da0cfecdbd14510ad7d
fevinfelix <fevinfelix@gmail.com>
2023-01-17 13:39:22 -0500

    can add and fetch login info from websites (even with multiple accounts from the same website)

--- main.py
+++ main.py
@@ -3,32 +3,27 @@ import os
 username = None
 password = None
 
+websites = [] # list of website_logins items
+# str : [{user, pass}, {user, pass}, {user, pass}]
+
 site_passwords = {}
 
-class User:
-    def __init__(self, username: str, password: str):
-        self.username = username
-        self.password = password
-        self.password_pairs = {}
+class website_logins: # represents the all the different login info for a specific website
+    def __init__(self, name: str):
+        self.name = name
+        self.accounts = {}
+"""
+def add_password(site: str, password: str) -> bool:
+    if not site.lower in site_passwords: # if the site hasn't been added yet
+        site_passwords[site.lower] = [password]
+    else: # we have to check if the same password has already been added too 
+        
 
-    def add_password(self, site: str, password: str) -> bool:
-        if site.lower in self.password_pairs:
-            return False
-        else:
-            self.password_pairs[site.lower] = password
-            return True
-    
-    def remove_password(self, site: str) -> bool:
-        if self.password_pairs.pop(site.lower, -1) == -1:
-            return False
-        return True        
-    
-    def change_password(self, site: str, new_password: str) -> bool:
-        if site.lower in self.password_pairs:
-            self.password_pairs.update({site, new_password})
-            return True
+def remove_password(site: str) -> bool:
+    if site_passwords.pop(site.lower, -1) == -1:
         return False
-
+    return True        
+"""
 if os.path.getsize('MasterLogin.txt') != 0: # if a profile has been set up already, load in the username and password for it
     with open("MasterLogin.txt") as f:
         (key, val) = f.readline().strip('\n').split()
@@ -37,11 +32,12 @@ if os.path.getsize('MasterLogin.txt') != 0: # if a profile has been set up alrea
 
 def change_master_login():
     global username, password
-    entered_username = str(input("Please enter your current username "))
+    entered_username = str(input("Please enter your current username: "))
     entered_password = str(input("Please enter your current password: "))
     if entered_username == username and entered_password == password:
         username = str(input("Please enter a new username to use: "))
         password = str(input("Please enter a new password to use: "))
+        #os.system('cls')
         print("\nSuccessfully changed login information!")
     else:
         print("\nIncorrect username or password was entered. Please try again.\n")
@@ -75,7 +71,7 @@ def main():
     else: # if the profile was already created, prompt them to log in
         print("Welcome to your password manager!")
         while True:
-            entered_username = str(input("Please enter a username/email to login with: "))
+            entered_username = str(input("Please enter your username/email to login with: "))
             entered_password = str(input("Please enter the password to login: "))
             if entered_username == username and entered_password == password:
                 break
@@ -87,44 +83,65 @@ def main():
     quit = False
 
     while quit == False:
-        print("Here are the sites you currently have passwords for: ")
-        for site in site_passwords:
-            print(site)
+        print("Here are the sites you currently have login info for: ")
+        for site in websites:
+            print(site.name)
 
         choice = after_login_choices() # returns either 'f' 'a' 'c' 'd' or 'r' for fetch password, add password, change password, delete password, or reset login 
         
         if choice == 'f':
-            if len(site_passwords) == 0:
-                print("\nMust have at least one password added to fetch.")
+            if len(websites) == 0:
+                print("\nMust have login info for at least one website added to fetch.")
             else:
-                site_choice = None
-                while True:
-                    site_choice = str(input("For which site do you want to access passwords for?: "))
-                    found_site = False
-                    for site in site_passwords.keys():
-                        if site == site_choice:
-                            found_site == True
-                            break
-                    if found_site == True:
+                site_logins = None
+                site_choice = str(input("For which site do you want to access usernames and passwords for?: ")).lower()
+                found_site = False
+                for site in websites:
+                    if site.name == site_choice:
+                        found_site = True
+                        site_logins = site
                         break
-                    print("Please enter a site that you currently have a password stored for.")
-                for site in site_passwords:
-                    if site == site_choice:
-                        for u, p in site_passwords.get(site_choice).items():   # looping through each username and password entry in that site (dictionary)
-                            print(f"Username: {u}   Password: {p}") # prints usernames and passwords for that site
+                if found_site == True:
+                    for u, p in site_logins.accounts.items():
+                        print(f"Username: {u}   Password: {p}") # prints usernames and passwords for that site
+                else: 
+                    print("You do not currently have login info stored for that website.")
         elif choice == 'r':
             change_master_login()
-        elif choice == 'a':
+        elif choice == 'a' or 'c':
+            if choice == 'c' and len(websites) == 0:
+                print("\nMust have login info for at least one website added to change login information.")
+            else:
+                site_choice = str(input("What site would you like to add/change username and password for?: ")).lower()
+                new_username = str(input("What username would you like to add/change?: ")).lower()
+                new_password = str(input("What is the new password for that username?: "))
+                site_logins = None
+                found_site = False
+                for site in websites:
+                    if site.name == site_choice:
+                        found_site = True
+                        site_logins = site
+                        break
+                if found_site == True: # if the site already has login info stored
+                    print("did this")
+                    site_logins.accounts[new_username] = new_password
+                else: # create a new website_logins object for the login and add it to the list of known websites
+                    new_website = website_logins(site_choice)
+                    new_website.accounts[new_username] = new_password
+                    websites.append(new_website)
+                print(f"Successfully added/changed login info for {site_choice}!")
+        
+        else: # must be 'd' for delete password
             quit = True
 
+            
 
+    user_file = open("MasterLogin.txt", "w") # clears the current data in the MasterLogin.txt file before uploading new data
+    user_file = open("MasterLogin.txt", "a")
 
+    user_file.write(username + " " + password)
 
-    """
-    users_file = open("users.txt", "w") # clears the current data in the users.txt file before uploading data from users list
-    users_file = open("users.txt", "a")
- 
-    for user in users: # saves the new data to text files before exiting password manager
-        users_file.write(user.username + " " + user.password + "\n")
-    """
+    print(websites.items())
+    
+
 main()
